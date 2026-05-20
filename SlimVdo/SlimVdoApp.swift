aa263d583fc08726b351ad90b6e75df413ea7c4e
Draymski <jamesray0924@gmail.com>
2026-05-21 06:46:58 +0800

    Initial Commit

---
+++ SlimVdo/SlimVdoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlimVdoApp.swift
+//  SlimVdo
+//
+//  Created by Raymond Holmes on 2026/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SlimVdoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
